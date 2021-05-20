01281b6c7fad9b689f5bcda117dd526027af2284
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-20 21:51:47 +0200

    Add files via upload

---
+++ src/topnav.js
@@ -0,0 +1,14 @@
+function TopNav() {
+  return (
+    <div>
+      <ul className="navbar">
+        <li className="list-item">About Me</li>
+        <li className="list-item">Projects</li>
+        <li className="list-item">Resume</li>
+        <li className="list-item">Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default TopNav
