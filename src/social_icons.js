52507e29ad95532888d61a1dbcfed7460dee6ffa
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-21 19:51:18 +0200

    Add files via upload

---
+++ src/social_icons.js
@@ -0,0 +1,13 @@
+import { SocialIcon } from 'react-social-icons'
+
+export default function Social() {
+  return (
+    <div id="social_icons_container">
+      <SocialIcon url="https://twitter.com/AmineAouragh20" className="social_icon"/>
+      <SocialIcon url="https://linkedin.com/in/amineaouragh" className="social_icon"/>
+      <SocialIcon url="https://github.com/AmineAouragh" className="social_icon"/>
+      <SocialIcon url="https://instagram.com/amineaouragh21" className="social_icon"/>
+      <SocialIcon url="https://facebook.com/AmineAouragh20" className="social_icon"/>
+    </div>
+  )
+}
