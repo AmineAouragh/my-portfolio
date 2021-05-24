961edd752e8148d5de4796bdb9f526fed8a9e2f1
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-24 21:48:32 +0200

    Add files via upload

---
+++ src/about.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div className="main-section">
+       <p className="name">AMINE AOURAGH</p>
+       <p className="role">
+          Web Designer <br />
+          Android Developer <br />
+          React Native Developer
+       </p>
+    </div>
+  )
+}
