e719359e1efb11df55c49319167647690191cf93
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-25 12:27:06 +0200

    Fix fade in effect

--- src/about.js
+++ src/about.js
@@ -2,7 +2,7 @@ import FadeIn from 'react-fade-in'
 
 export default function About() {
   return (
-    <FadeIn className="main-section" transitionDuration={700} delay={500}>
+    <FadeIn className="main-section" transitionDuration={1000} delay={2500}>
        <p className="name">AMINE AOURAGH</p>
        <p className="role">
           Web Designer <br />
