db894a67338cc0125a59ec7f39a14980f80cb513
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-28 20:51:43 +0100

    Add files via upload

--- src/about.js
+++ src/about.js
@@ -2,6 +2,8 @@
 export default function About() {
   return (
     <div className="main-section">
+       <div className="photo">
+       </div>
        <p className="name">AMINE AOURAGH</p>
        <p className="role">
           Web Designer <br />
