00ca940c858d967b5d70bb3174cb74bfc6528bfa
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-30 20:28:27 +0100

    Add files via upload

--- src/skills.js
+++ src/skills.js
@@ -9,15 +9,14 @@ import bootstrap from './bootstrap-image.png'
 export default function Skills() {
   return (
     <div className="section-skills">
-      <h1 className="main-title">Skills</h1>
-      <h2>Web Technologies</h2>
+      <h1 className="main-title">My Tech Stack</h1>
       <div>
         <img src={html} alt="HTML" width={55} height={60}/>
-        <img src={css} alt="CSS" width={45} height={60} />
-        <img src={js} alt="Javascript" width={50} height={50} />
+        <img src={css} alt="CSS" width={50} height={54} />
+        <img src={js} alt="Javascript" width={55} height={55} />
         <img src={ts} alt="Typescript" width={50} height={50} />
         <img src={scss} alt="SCSS" width={70} height={50} />
-        <img src={bootstrap} alt="Bootstrap" width={60} height={50} />
+        <img src={bootstrap} alt="Bootstrap" width={65} height={56} />
         <img src={react} alt="React" width={80} height={50} />
       </div>
     </div>
