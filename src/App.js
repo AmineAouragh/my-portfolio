61ebf3d528b952deff60507961936c38fef0e113
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-06-03 14:29:59 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TopNav from './topnav'
 import About from './about'
 import Skills from './skills'
+import Social from './social_icons'
 import FadeIn from 'react-fade-in'
 import './App.scss'
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <FadeIn transitionDuration={2000} delay={1000}>
       <TopNav />
+      <Social />
       <About />
       <Skills />
     </FadeIn>
